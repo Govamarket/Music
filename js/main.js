0c9e6c9d96db58d969293a6eb88cd2e46ec6e6a7
Govamarket <govaway@gmail.com>
2022-09-10 13:37:29 +0100

    Music

---
+++ js/main.js
@@ -0,0 +1,10 @@
+// var myName = 'Clinton';
+// let lastName ='Chidera';
+// console.log(myName + lastName);
+
+// alert('Hello client');
+
+// let= alert('I am bot to assit you');
+
+// prompt ('what is you Name');
+// console.log= prompt('How many we assit you?');
